6dfe09adbba28cd071fbafe1e410b29b9ed28c32
Jardson Alan <jardsonalan132@gmail.com>
2025-07-17 20:49:57 -0300

    feat: criando models de pagamentos e adicionando relacionamento com pedidos

---
+++ app/models/pagamento.ts
@@ -0,0 +1,34 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+enum MetodoPagamento {
+  'CARTAO_CREDITO',
+  'PIX',
+  'BOLETO',
+}
+
+enum Status {
+  'ESPERA',
+  'COMPLETO',
+  'FALHOU',
+}
+
+export default class Pagamento extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare metodosPagamentos: MetodoPagamento
+
+  @column()
+  declare status: Status
+
+  @column()
+  declare valorPagamento: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
--- app/models/pedido.ts
+++ app/models/pedido.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import Pagamento from '#models/pagamento'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
 
 enum Status {
   'ESPERA',
@@ -17,6 +19,9 @@ export default class Pedido extends BaseModel {
 
   @column()
   declare total: number
+
+  @hasMany(() => Pagamento)
+  declare pagamento: HasMany<typeof Pagamento>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
