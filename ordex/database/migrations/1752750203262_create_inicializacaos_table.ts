f0d7e5391bcb8af38711f32a048f0189f48901c8
Barr0ca <iandearaujogalvao@gmail.com>
2025-07-17 17:13:27 -0300

    Criando o restante das migrações: pedidos, pagamentos, itens_pedidos e enderecos_entrega; Modificando as migrações produtos e categorias para serem equivalente ao esquemático;

--- ordex/database/migrations/1752750203262_create_inicializacaos_table.ts
+++ ordex/database/migrations/1752750203262_create_inicializacaos_table.ts
@@ -2,10 +2,10 @@ import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
   async up() {
-    this.schema.createTable('categoria', (table) => {
+    this.schema.createTable('categorias', (table) => {
       table.increments('id')
 
-      table.string('nome', 150).notNullable()
+      table.string('nome', 100).notNullable()
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
@@ -15,11 +15,65 @@ export default class extends BaseSchema {
       table.increments('id')
 
       table.string('nome', 150).notNullable()
-      table.string('descricao', 254).notNullable()
-      table.double('preco').notNullable()
-      table.integer('preco').notNullable()
-      
+      table.text('descricao').notNullable()
+      table.decimal('preco').notNullable()
+      table.integer('qtd_estoque').notNullable()
+
       table.integer('categoria_id').unsigned().references('categorias.id').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+
+    this.schema.createTable('pedidos', (table) => {
+      table.increments('id')
+
+      table.enum('status', ['ESPERA', 'ROTA_ENTREGA', 'CANCELADO', 'ENTREGUE']).notNullable()
+      table.decimal('total').notNullable()
+
+      table.integer('usuario_id').unsigned().references('usuarios.id').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+
+    this.schema.createTable('pagamentos', (table) => {
+      table.increments('id')
+
+      table.enum('metodos_pagamento', ['CARTAO_CREDITO', 'PIX', 'BOLETO']).notNullable()
+      table.enum('status', ['ESPERA', 'COMPLETO', 'FALHOU']).notNullable()
+      table.decimal('valor_pagamento').notNullable()
+
+      table.integer('pedido_id').unsigned().references('pedidos.id').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+
+    this.schema.createTable('itens_pedido', (table) => {
+      table.increments('id')
+
+      table.integer('quantidade').notNullable()
+      table.decimal('preco_unitario').notNullable()
+
+      table.integer('pedido_id').unsigned().references('pedidos.id').notNullable()
+      table.integer('produto_id').unsigned().references('produtos.id').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+
+    this.schema.createTable('enderecos_entrega', (table) => {
+      table.increments('id')
+
+      table.string('endereco', 255).notNullable()
+      table.string('cidade', 100).notNullable()
+      table.string('estado', 100).notNullable()
+      table.string('caixa_postal', 50).notNullable()
+      table.string('pais', 100).notNullable()
+
+      table.integer('pedido_id').unsigned().references('pedidos.id').notNullable()
+      table.integer('usuario_id').unsigned().references('usuarios.id').notNullable()
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
@@ -27,6 +81,11 @@ export default class extends BaseSchema {
   }
 
   async down() {
+    this.schema.dropTable('categoria')
     this.schema.dropTable('produtos')
+    this.schema.dropTable('pedidos')
+    this.schema.dropTable('pagamentos')
+    this.schema.dropTable('itens_pedido')
+    this.schema.dropTable('enderecos_entrega')
   }
 }
