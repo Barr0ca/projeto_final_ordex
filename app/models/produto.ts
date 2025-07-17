d5024ab1472295a74a88008b58f576cf30330872
Jardson Alan <jardsonalan132@gmail.com>
2025-07-17 19:29:54 -0300

    feat: criando models de produtos

---
+++ app/models/produto.ts
@@ -0,0 +1,30 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, manyToMany } from '@adonisjs/lucid/orm'
+import Categoria from '#models/categoria'
+import type { ManyToMany } from '@adonisjs/lucid/types/relations'
+
+export default class Produto extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare nome: string
+
+  @column()
+  declare descricao: string
+
+  @column()
+  declare preco: number
+
+  @column()
+  declare quatidade_estoque: number
+
+  @manyToMany(() => Categoria)
+  declare categoria: ManyToMany<typeof Categoria>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
