36529e11434e2412e51ab3f430a8fa10a13074c3
Jardson Alan <jardsonalan132@gmail.com>
2025-07-18 22:28:44 -0300

    feat: adicionando relacionamento manyToMany no model de produtos

--- app/models/produto.ts
+++ app/models/produto.ts
@@ -2,6 +2,7 @@ import { DateTime } from 'luxon'
 import { BaseModel, column, manyToMany } from '@adonisjs/lucid/orm'
 import Categoria from '#models/categoria'
 import type { ManyToMany } from '@adonisjs/lucid/types/relations'
+import Pedido from '#models/pedido'
 
 export default class Produto extends BaseModel {
   @column({ isPrimary: true })
@@ -23,6 +24,13 @@ export default class Produto extends BaseModel {
     pivotTable: 'categoria_produto',
   })
   declare categoria: ManyToMany<typeof Categoria>
+
+  @manyToMany(() => Pedido, {
+    pivotTable: 'itens_pedido',
+    pivotColumns: ['quantidade', 'preco_unitario'],
+    pivotTimestamps: true,
+  })
+  declare produto: ManyToMany<typeof Pedido>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
