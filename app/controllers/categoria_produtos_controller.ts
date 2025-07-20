5c04640f01bc813a07f5113b3b0b6687426cfcc5
Jardson Alan <jardsonalan132@gmail.com>
2025-07-20 15:04:34 -0300

    chore: atualização no método attach do relacionamento de categorias com produtos

--- app/controllers/categoria_produtos_controller.ts
+++ app/controllers/categoria_produtos_controller.ts
@@ -1,10 +1,12 @@
 import type { HttpContext } from '@adonisjs/core/http'
-import Produto from '#models/produto'
+import Categoria from '#models/categoria'
 
 export default class CategoriaProdutosController {
-  async associarProduto({ params }: HttpContext) {
-    const produto = await Produto.find(params.id)
+  async associarProduto({ params, request }: HttpContext) {
+    const categoria = await Categoria.findOrFail(params.id)
 
-    await produto?.related('categoria').attach([produto.id])
+    await categoria?.related('produto').attach(request.input('produto'))
+    await categoria?.load('produto')
+    return categoria
   }
 }
