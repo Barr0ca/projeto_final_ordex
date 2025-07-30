33f5ef6b26ae91285ee5e41443acc74366e6d577
Jardson Alan <jardsonalan132@gmail.com>
2025-07-30 20:04:42 -0300

    feat: adicionando as rotas da aplicação - Closes: #12

--- start/routes.ts
+++ start/routes.ts
@@ -1,16 +1,28 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+const CategoriasController = () => import('#controllers/categorias_controller')
+const EnderecosEntregaController = () => import('#controllers/enderecos_entrega_controller')
+const PagamentosController = () => import('#controllers/pagamentos_controller')
+const PedidosController = () => import('#controllers/pedidos_controller')
+const ProdutosController = () => import('#controllers/produtos_controller')
+
+const CategoriaProdutosController = () => import('#controllers/categoria_produtos_controller')
+const ItensPedidosController = () => import('#controllers/itens_pedidos_controller')
+
+router.resource('/categoria', CategoriasController).except(['create', 'edit'])
+router.resource('/endereco-entrega', EnderecosEntregaController).except(['create', 'edit'])
+router.resource('/pagamento', PagamentosController).except(['create', 'edit'])
+router.resource('/pedido', PedidosController).except(['create', 'edit'])
+router.resource('/produto', ProdutosController).except(['create', 'edit'])
+
+router.post('/categoria/:id/associar-produto', [
+  CategoriaProdutosController,
+  'associarCategoriaProdutos',
+])
+router.post('/categoria/:id/desassociar-produto', [
+  CategoriaProdutosController,
+  'desassociarCategoriaProdutos',
+])
+
+router.post('/pedido/:id/adicionar-pedido', [ItensPedidosController, 'associarItensPedidos'])
+router.post('/pedido/:id/retirar-pedido', [ItensPedidosController, 'desassociarItensPedidos'])
