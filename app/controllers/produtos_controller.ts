891ef8493e7d49a76d2d70b3734d1703cd7d9f34
Jardson Alan <jardsonalan132@gmail.com>
2025-07-30 14:02:45 -0300

    chore: ajustando indentação dos códigos da controller de produtos

--- app/controllers/produtos_controller.ts
+++ app/controllers/produtos_controller.ts
@@ -5,18 +5,21 @@ export default class ProdutosController {
   public async index({}: HttpContext) {
     return await Produto.query().preload('categoria')
   }
+
   public async show({ params }: HttpContext) {
     return await Produto.query().where('id', params.id).preload('categoria').firstOrFail()
   }
+
   public async store({ request }: HttpContext) {
     return await Produto.create(request.only(['nome', 'descricao', 'preco', 'quantidadeEstoque']))
   }
+
   public async update({ request, params }: HttpContext) {
     const produto = await Produto.findOrFail(params.id)
     produto.merge(request.only(['nome', 'descricao', 'preco', 'quantidadeEstoque']))
-    await produto.save()
-    return produto
+    return await produto.save()
   }
+
   public async destroy({ params }: HttpContext) {
     const produto = await Produto.findOrFail(params.id)
     await produto.delete()
