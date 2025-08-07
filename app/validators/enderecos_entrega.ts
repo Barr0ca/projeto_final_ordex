bcb25d31706d4a7ab72b75b48cb1c0ce94fb2a8d
uluscaz-ifrn <dantas.lucas2@escolar.ifrn.edu.br>
2025-08-07 18:14:46 -0300

    style: formataçao do do código

--- app/validators/enderecos_entrega.ts
+++ app/validators/enderecos_entrega.ts
@@ -2,13 +2,13 @@ import vine from '@vinejs/vine'
 
 export const createEnderecoEntregaValidator = vine.compile(
     vine.object({
-    endereco: vine.string().trim().maxLength(255),
-    cidade: vine.string().trim().maxLength(100),
-    estado: vine.string().trim().maxLength(100),
-    caixaPostal: vine.string().trim().maxLength(20),
-    pais: vine.string().trim().maxLength(100),
-    pedidoId: vine.number().positive().min(1),
-    usuarioId: vine.number().positive().min(1)
+        endereco: vine.string().trim().maxLength(255),
+        cidade: vine.string().trim().maxLength(100),
+        estado: vine.string().trim().maxLength(100),
+        caixaPostal: vine.string().trim().maxLength(20),
+        pais: vine.string().trim().maxLength(100),
+        pedidoId: vine.number().positive().min(1),
+        usuarioId: vine.number().positive().min(1)
     })
 )
 
