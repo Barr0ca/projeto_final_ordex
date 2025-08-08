9cb69a7d307b0756106e7fa91c10f1027d3b650a
Jardson Alan <jardsonalan132@gmail.com>
2025-08-07 22:05:48 -0300

    chore: corrigindo validadores de enderecos_entrega e reorganizando a lógica do código e os imports da controller de enderecos_entrega - Closes: #16

--- app/controllers/enderecos_entrega_controller.ts
+++ app/controllers/enderecos_entrega_controller.ts
@@ -1,6 +1,10 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import EnderecoEntrega from '#models/endereco_entrega'
-import { createEnderecoEntregaValidator, updateEnderecoEntregaValidator } from '#validators/enderecos_entrega'
+import {
+  createEnderecoEntregaValidator,
+  updateEnderecoEntregaValidator,
+} from '#validators/enderecos_entrega'
+
 export default class EnderecoEntregasesController {
   async index({}: HttpContext) {
     return await EnderecoEntrega.all()
@@ -18,9 +22,7 @@ export default class EnderecoEntregasesController {
   async update({ params, request }: HttpContext) {
     const payload = await request.validateUsing(updateEnderecoEntregaValidator)
     const endereco = await EnderecoEntrega.findOrFail(params.id)
-    endereco.merge(payload)
-    return await endereco.save()
-    
+    return await endereco.merge(payload).save()
   }
 
   async destroy({ params }: HttpContext) {
